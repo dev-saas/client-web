2101fc4dfe5fbffa7437e200d89d071d7bb551dc
Rafael <rafaelnsantos1@gmail.com>
2019-07-20 15:16:21 -0300

    remove test

--- features/auth.feature
+++ features/auth.feature
@@ -5,10 +5,3 @@ Feature: Authentication
         And Eu digito a senha "test"
         And Eu clico submit
         Then Eu devo ver o botao de logout
-
-    Scenario: Realizar login password errado
-        Given Eu estou na pagina de login
-        When Eu digito o email "test@test.com"
-        And Eu digito a senha "teste"
-        And Eu clico submit
-        Then Eu devo ver o erro de senha
--- features/step_definitions/login.js
+++ features/step_definitions/login.js
@@ -4,26 +4,22 @@ const AuthPage = require('../page-objects/auth')
 
 let page
 
-Given('Eu estou na pagina de login', function () {
+Given('Eu estou na pagina de login', function() {
   page = new AuthPage(this.driver)
 })
 
-When('Eu digito o email {string}', function (email) {
+When('Eu digito o email {string}', function(email) {
   page.email = email
 })
 
-When('Eu digito a senha {string}', function (password) {
+When('Eu digito a senha {string}', function(password) {
   page.password = password
 })
 
-When('Eu clico submit', function () {
+When('Eu clico submit', function() {
   return page.submitForm()
 })
 
-Then('Eu devo ver o botao de logout', async function () {
+Then('Eu devo ver o botao de logout', async function() {
   return (await page.asyncFind(By.id('logout'))).isDisplayed()
-})
-
-Then('Eu devo ver o erro de senha', async function () {
-  return (await page.asyncError).isDisplayed()
 })
